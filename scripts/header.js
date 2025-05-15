adbb22c432be1516e47278a71683387196961db6
digikwal <digikwal@users.noreply.github.com>
2025-05-15 22:30:35 +0200

    feat: Improve mobile navigation functionality; toggle visibility and enhance styles

--- scripts/header.js
+++ scripts/header.js
@@ -3,16 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(res => res.text())
         .then(html => {
             document.getElementById("header").innerHTML = html;
-            // Initialiseer menu.js hier
+
             const hamburgerMenu = document.querySelector('.hamburger-menu');
             const mobileNav = document.querySelector('.mobile-nav');
 
+            // Zorg dat het mobiele menu standaard verborgen is
             if (!mobileNav.classList.contains('hidden')) {
                 mobileNav.classList.add('hidden');
             }
 
+            // Toggle het mobiele menu bij klikken op het hamburger menu
             hamburgerMenu.addEventListener('click', () => {
                 mobileNav.classList.toggle('hidden');
+                mobileNav.classList.toggle('open');
             });
         });
 });
