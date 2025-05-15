3215036961ac99b5fa86173c39643132887b7090
digikwal <digikwal@users.noreply.github.com>
2025-05-15 22:12:47 +0200

    feat: Remove menu.js and integrate mobile navigation functionality into header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch("/partials/header.html")
-    .then(res => res.text())
-    .then(html => document.getElementById("header").innerHTML = html);
+    fetch("/partials/header.html")
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById("header").innerHTML = html;
+            // Initialiseer menu.js hier
+            const hamburgerMenu = document.querySelector('.hamburger-menu');
+            const mobileNav = document.querySelector('.mobile-nav');
+
+            if (!mobileNav.classList.contains('hidden')) {
+                mobileNav.classList.add('hidden');
+            }
+
+            hamburgerMenu.addEventListener('click', () => {
+                mobileNav.classList.toggle('hidden');
+            });
+        });
 });
--- scripts/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const mobileNav = document.querySelector('.mobile-nav');
-  
-    hamburgerMenu.addEventListener('click', () => {
-      mobileNav.classList.toggle('hidden'); // Toggle de 'hidden' klasse
-    });
-  });
